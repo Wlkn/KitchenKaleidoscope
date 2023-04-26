290ce048333140219c447e87aa14c4069016e5e8
Wlkn <alibachar27@gmail.com>
2023-04-26 11:51:24 -0400

    Changed to neonDB

--- BackEnd/postGres.ts
+++ BackEnd/postGres.ts
@@ -5,10 +5,10 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
     connectionTimeoutMillis: 200000,
     idleTimeoutMillis: 300000,
-    host: process.env.POSTGRE_HOST_STRING,
-    user: process.env.POSTGRE_USER,
-    password: process.env.POSTGRE_PASSWORD,
-    database: process.env.POSTGRE_DATABASE, //KitchenKaleidoscope Database
+    host: process.env.POSTGRE_HOST_STRING2,
+    user: process.env.POSTGRE_USER2,
+    password: process.env.POSTGRE_PASSWORD2,
+    database: process.env.POSTGRE_DATABASE2, //KitchenKaleidoscope Database
     
     //switched to "better" free database than before since it was slow and was going to expire soon
     port: 5432,
